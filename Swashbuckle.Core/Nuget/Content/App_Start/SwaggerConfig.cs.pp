0457981fc115e346134897749ad55e986690404f
Javier Feston <jfeston@gmail.com>
2016-12-28 11:43:51 -0800

    Add SwaggerConfig template

---
+++ Swashbuckle.Core/Nuget/Content/App_Start/SwaggerConfig.cs.pp
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebActivatorEx;
+using $rootnamespace$;
+using Swashbuckle.Application;
+using Swashbuckle.Swagger;
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace $rootnamespace$
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            var thisAssembly = typeof(SwaggerConfig).Assembly;
+
+            GlobalConfiguration.Configuration 
+                .EnableSwagger(c =>
+                    {
+                        c.RootUrl(GetRootUrl);
+                        c.Schemes(new[] { "https" });
+                        c.SingleApiVersion("v1", "Auth Server")
+                            .Description("")
+                            .TermsOfService("Terms of Service sample");
+                        c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.DescribeAllEnumsAsStrings();
+                    });
+        }
+
+		private static string GetRootUrl(HttpRequestMessage req)
+		{
+			return new Uri(req.RequestUri, req.GetRequestContext().VirtualPathRoot).ToString();
+		}
+
+		private static string GetXmlCommentsPath() => $@"{AppDomain.CurrentDomain.BaseDirectory}\bin\$rootnamespace$.XML";
+	}
+}
